b57263b5d73b526b58ee8b178870703c0fe1f834
dubey748 <dubeyabhishek748@gmail.com>
2023-08-29 22:05:54 +0530

    Testing..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-orange-500 text-8xl'>App</div>
+    <div className='text-orange-500 text-8xl'>Hello</div>
   )
 }
 
