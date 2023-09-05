246fe2823c85ad61615b27d7bf117c32a02e972c
dubey748 <dubeyabhishek748@gmail.com>
2023-09-05 14:17:14 +0530

    Login and signup page created and routing is also setup between them

---
+++ frontend/src/pages/Signuppage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../features/auth/components/Signup";
+
+const Signuppage = () => {
+  return (
+    <>
+      <Signup />
+    </>
+  );
+};
+
+export default Signuppage;
