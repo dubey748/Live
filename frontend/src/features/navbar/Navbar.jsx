50b6446de282b07e79b52f7ab18110b859a024f4
Ritesh <ritesh.kedar06@gmail.com>
2023-09-13 21:52:20 +0530

    Fix the issue by...

--- frontend/src/features/navbar/Navbar.jsx
+++ frontend/src/features/navbar/Navbar.jsx
@@ -169,40 +169,43 @@ const Navbar = ({ children }) => {
                   ))}
                 </div>
                 <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        {user.name}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {user.email}
-                      </div>
-                    </div>
-
-                    <Link to="/cart">
-                      <button
-                        type="button"
-                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-
-                        <ShoppingCartIcon
-                          className="h-6 w-6"
-                          aria-hidden="true"
+                  <div className="flex justify-between  items-center px-5">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={user.imageUrl}
+                          alt=""
                         />
-                      </button>
-                    </Link>
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium leading-none text-white">
+                          {user.name}
+                        </div>
+                        <div className="text-sm font-medium leading-none text-gray-400">
+                          {user.email}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center">
+                      <Link to="/cart">
+                        <button
+                          type="button"
+                          className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                          <span className="absolute -inset-1.5" />
 
-                    <span className="inline-flex items-center rounded-md mb-7 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                      0
-                    </span>
+                          <ShoppingCartIcon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        </button>
+                      </Link>
+
+                      <span className="inline-flex items-center rounded-md mb-7 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                        0
+                      </span>
+                    </div>
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
