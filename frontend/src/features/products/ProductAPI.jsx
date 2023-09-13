c142924751f1f6e8fac0b9cfa5f3bcc835275da7
dubey748 <dubeyabhishek748@gmail.com>
2023-09-13 22:48:29 +0530

    Filter-functionality is  working properly

--- frontend/src/features/products/ProductAPI.jsx
+++ frontend/src/features/products/ProductAPI.jsx
@@ -1,8 +1,25 @@
 export function fetchAllProducts() {
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:3000/products') 
-      const data = await response.json()
-      resolve({data})
-    }
-    );
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:3000/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter) {
+  //filter ={"category":"name"}
+
+  let queryString = '';
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}`;
   }
+
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      `http://localhost:3000/products?${queryString}`
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
+}
