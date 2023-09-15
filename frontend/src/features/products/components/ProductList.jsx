35645f3ac2482136792f7a1808a4c834332ced17
ABhishek Dubey <65311526+dubey748@users.noreply.github.com>
2023-09-15 20:57:47 +0530

    Update ProductList.jsx

--- frontend/src/features/products/components/ProductList.jsx
+++ frontend/src/features/products/components/ProductList.jsx
@@ -336,7 +336,7 @@ export default function ProductList() {
 
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 
-            <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-5 gap=[30px]">
+            <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-5 gap-[30px]">
 
               <h1 className="text-4xl font-bold tracking-tight text-gray-900">
                 Let's Explore
