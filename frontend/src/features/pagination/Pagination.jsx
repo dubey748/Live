a92ab249bcb4ac1a1a5bf1073a62669c9280de01
dubey748 <dubeyabhishek748@gmail.com>
2023-09-20 15:33:37 +0530

    Pagination in Mobile version is working fine also Left icon and right icon will redirect to previous and next page

--- frontend/src/features/pagination/Pagination.jsx
+++ frontend/src/features/pagination/Pagination.jsx
@@ -2,28 +2,46 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 import { ITEMS_PER_PAGE } from "../../app/constants";
 
-export default function Pagination({
-  handlePage,
-  page,
-  setPage,
-  totalItems,
-}) {
+export default function Pagination({ handlePage, page, setPage, totalItems }) {
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+  const handlePrevious = () => {
+    if (page > 1) {
+      handlePage(page - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      handlePage(page + 1);
+    }
+  };
+
   return (
     <div className=" flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        <button
+          onClick={handlePrevious}
+          disabled={page === 1}
+          className={`relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 ${
+            page === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-50"
+          }`}
         >
           Previous
-        </a>
-        <a
-          href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        </button>
+        <button
+          onClick={handleNext}
+          disabled={page === totalPages}
+          className={`relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 ${
+            page === totalPages
+              ? "opacity-50 cursor-not-allowed"
+              : "hover:bg-gray-50"
+          }`}
         >
           Next
-        </a>
+        </button>
       </div>
+
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
@@ -40,13 +58,17 @@ export default function Pagination({
             className="isolate inline-flex -space-x-px rounded-md shadow-sm"
             aria-label="Pagination"
           >
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            <div
+              onClick={handlePrevious}
+              className={`cursor-pointer relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${
+                page === 1 ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              aria-hidden="true"
             >
               <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+              <ChevronLeftIcon className="h-5 w-5" />
+            </div>
+
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
 
             {Array.from({ length: Math.ceil(totalItems / ITEMS_PER_PAGE) }).map(
@@ -65,13 +87,16 @@ export default function Pagination({
               )
             )}
 
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            <div
+              onClick={handleNext}
+              className={`cursor-pointer relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${
+                page === totalPages ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              aria-hidden="true"
             >
               <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+              <ChevronRightIcon className="h-5 w-5" />
+            </div>
           </nav>
         </div>
       </div>
